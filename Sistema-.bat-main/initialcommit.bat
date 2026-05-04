e9ec05384139f08cbe3a163b8dcc9582e3e827ef
JOSE-code67 <jmagalhaesdefaria@gmail.com>
2026-05-04 18:48:35 -0300

    Add files via upload
    cara num sei como adicionar essas alterações, mas eu melhorei o bagulho da pessoa selecionar a cor, tá mais completo e funcionando(teoricamente)

--- Sistema-.bat-main/initialcommit.bat
+++ Sistema-.bat-main/initialcommit.bat
@@ -8,18 +8,44 @@ echo ===========================================================================
 echo                          SELECIONE O TEMA VISUAL
 echo ================================================================================
 echo.
-echo   [1] Texto Azul e Tela Preta
-echo   [2] Texto Preto e Tela Azul
+echo   [1] Texto Azul e Tela Preta/ [10] Texto Preto e Tela Azul
+echo   [2] Texto Verde e Tela Preta/ [20] Texto Preto e Tela Verde
+echo   [3] Texto Azul Claro e Tela Preta/ [30] Texto Preto e Tela Azul Claro
+echo   [4] Texto Vermelho e Tela Preta/ [40] Texto Preto e Tela Vermelha
+echo   [5] Texto Roxo e Tela Preta/ [50] Texto Preto e Tela Roxa
+echo   [6] Texto Amarelo e Tela Preta/ [60] Texto Preto e Tela Amarela
+echo   [7] Texto Branco e Tela Preta/ [70] Texto Preto e Tela Branca
+echo   [8] Texto Cinza e Tela Preta/ [80] Texto Preto e Tela Cinza
+echo   [9] Texto AzulRush e Tela Branca/ [90] Texto Branco e Tela AzulRush
 echo.
 set /p cor=Digite sua opcao: 
 
-if "%cor%"=="1" color 09
-if "%cor%"=="2" color 10
+if "%cor%"=="1" color 01
+goto menu
+if "%cor%"=="2" color 02
+goto menu
+if "%cor%"=="3" color 03
+goto menu
+if "%cor%"=="4" color 04
+goto menu
+if "%cor%"=="5" color 05
+goto menu
+if "%cor%"=="6" color 06
+goto menu
+if "%cor%"=="7" color 07
+goto menu
+if "%cor%"=="8" color 08
+goto menu
+if "%cor%"=="9" color 09
+goto menu
+if "%cor%"=="10" color 10
+goto menu
 
 :menu
-
+color %cor%
 
 ::oi para eu te salvar:"ctrl+S" abrir terminal:"ctrl+"""
+::git config --global --add safe.directory D:/Sistema-.bat= da permissão ao arquivo
 cls
 echo.
 echo                ===================================================
